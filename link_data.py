aa5bd09d22eeb9ef44ce969d2f1db34c135b71fc
Jack <jack.grahl@yahoo.co.uk>
2016-03-02 22:24:52 +0000

    Add file to generate input data for dancing links.

---
+++ link_data.py
@@ -0,0 +1,13 @@
+def Rectangle(object):
+    def __init__(self, w, h, spare=5):
+        self.w = w
+        self.h = h
+        self.spare = spare
+
+    def rows(self):
+        for d in self.directions:
+            for x in range(self.w):
+                for y in range(self.h):
+                    if all([ self.valid(x+p[0], y+p[1]) for p in self.shapes[self.d] ])
+                        yield self.translate_row(x, y, p)
+
