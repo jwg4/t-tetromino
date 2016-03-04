6ab475bcf0b721fea275306a0e5b5a97737b2600
Jack <jack.grahl@yahoo.co.uk>
2016-03-04 00:18:50 +0000

    Add test checking for tetromino row.

--- tests/test_link_data.py
+++ tests/test_link_data.py
@@ -13,3 +13,9 @@ class TestRowBuilding(unittest.TestCase):
         rows = r.row_list()
         row1 = [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
         self.assertIn(row1, rows)
+        
+    def test_tetromino_row_for_3_by_3_square(self):
+        r = Rectangle(3, 3)
+        rows = r.row_list()
+        row1 = [1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        self.assertIn(row1, rows)
