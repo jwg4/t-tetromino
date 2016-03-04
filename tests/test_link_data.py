023ce9fb8a04466896a8fb8bb1cc01bc07566b79
Jack <jack.grahl@yahoo.co.uk>
2016-03-04 00:13:55 +0000

    Split into two tests.

--- tests/test_link_data.py
+++ tests/test_link_data.py
@@ -3,9 +3,13 @@ import unittest
 from link_data import Rectangle
 
 class TestRowBuilding(unittest.TestCase):
-    def test_rows_for_3_by_3_square(self):
+    def test_number_of_rows_for_3_by_3_square(self):
         r = Rectangle(3, 3)
         rows = r.row_list()
         self.assertEqual(len(rows), 8 + 5 * 9)
+
+    def test_rows_for_3_by_3_square(self):
+        r = Rectangle(3, 3)
+        rows = r.row_list()
         row1 = [1, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0]
         self.assertIn(row1, rows)
