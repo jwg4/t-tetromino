3830be211ae5fcc7a7ac7e00b2b33d1b069f3678
Jack <jack.grahl@yahoo.co.uk>
2016-03-08 22:05:07 +0000

    Add the actual values of the table.

--- tests/test_table_output.py
+++ tests/test_table_output.py
@@ -6,12 +6,12 @@ class TestTableToHeader(unittest.TestCase):
     def test_basic_table_converted_to_header(self):
         self.maxDiff = None
         table = [
-            [ 0, 0, 0, 0, 0, 0, 0 ],
-            [ 0, 0, 0, 0, 0, 0, 0 ],
-            [ 0, 0, 0, 0, 0, 0, 0 ],
-            [ 0, 0, 0, 0, 0, 0, 0 ],
-            [ 0, 0, 0, 0, 0, 0, 0 ],
-            [ 0, 0, 0, 0, 0, 0, 0 ],
+            [ 0, 0, 1, 0, 1, 1, 0 ],
+            [ 1, 0, 0, 1, 0, 0, 1 ],
+            [ 0, 1, 1, 0, 0, 1, 0 ],
+            [ 1, 0, 0, 1, 0, 0, 0 ],
+            [ 0, 1, 0, 0, 0, 0, 1 ],
+            [ 0, 0, 0, 1, 1, 0, 1 ],
         ]
         names = [ "1", "2", "3", "4", "5", "6", "7" ]
         f = open('tests/files/table.h', 'r')
