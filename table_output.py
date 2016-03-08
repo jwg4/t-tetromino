09574f44ac9e9d9c5d354cef7e9c864740890926
Jack <jack.grahl@yahoo.co.uk>
2016-03-08 22:12:38 +0000

    Add all the other pieces of the header file.

--- table_output.py
+++ table_output.py
@@ -1,10 +1,21 @@
 def table_to_header_file(table, names):
     s = ""
     count = 0
+    row_names = []
     for row in table:
         name = 'row%s' % count
         values = ', '.join(str(i) for i in row)
         line = 'bool %s[] = { %s };\n' % (name, values)
         s = s + line
         count = count + 1
+        row_names.append(name)
+    table_dec = "bool **table = new bool*[%d]{\n" % (count, )
+    s = s + table_dec
+    row_list = ", ".join(row_names)
+    list_of_rows = "        %s\n" % (row_list, )
+    s = s + list_of_rows
+    s = s + "};\n\n"
+    quoted_names = ', '.join( '"' + n + '"' for n in names )
+    names_dec = "std::vector<std::string> names = { %s };\n" % (quoted_names, )
+    s = s + names_dec
     return s
