3377fdde6dd1aba8e9abf6940ff4ed0ee06efa1e
Jack <jack.grahl@yahoo.co.uk>
2016-03-08 22:02:22 +0000

    Remove unnecessary indents.

--- tests/files/table.h
+++ tests/files/table.h
@@ -1,11 +1,11 @@
-        bool row0[] = { 0, 0, 1, 0, 1, 1, 0 };
-        bool row1[] = { 1, 0, 0, 1, 0, 0, 1 };
-        bool row2[] = { 0, 1, 1, 0, 0, 1, 0 };
-        bool row3[] = { 1, 0, 0, 1, 0, 0, 0 };
-        bool row4[] = { 0, 1, 0, 0, 0, 0, 1 };
-        bool row5[] = { 0, 0, 0, 1, 1, 0, 1 };
-        bool **table = new bool*[6]{
-                row0, row1, row2, row3, row4, row5
-        };
+bool row0[] = { 0, 0, 1, 0, 1, 1, 0 };
+bool row1[] = { 1, 0, 0, 1, 0, 0, 1 };
+bool row2[] = { 0, 1, 1, 0, 0, 1, 0 };
+bool row3[] = { 1, 0, 0, 1, 0, 0, 0 };
+bool row4[] = { 0, 1, 0, 0, 0, 0, 1 };
+bool row5[] = { 0, 0, 0, 1, 1, 0, 1 };
+bool **table = new bool*[6]{
+        row0, row1, row2, row3, row4, row5
+};
 
-        std::vector<std::string> names = { "1", "2", "3", "4", "5", "6", "7" };
+std::vector<std::string> names = { "1", "2", "3", "4", "5", "6", "7" };
