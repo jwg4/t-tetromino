23fc4e81a3289311015ef385e3d51c116132ec08
Jack <jack.grahl@yahoo.co.uk>
2016-03-09 00:17:57 +0000

    Remove this test, this functionality done elsewhere.

--- tests/test_link_data.py
+++ tests/test_link_data.py
@@ -20,11 +20,6 @@ class TestRowBuilding(unittest.TestCase):
         row1 = [1, 1, 1, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
         self.assertIn(row1, rows)
 
-    def test_string_output(self):
-        r = Rectangle(3, 3)
-        s = r.row_dump()
-        self.assertIsNotNone(s)
-
 class TestColumnNames(unittest.TestCase):
     def test_number_of_column_names_for_3_by_3_square(self):
         r = Rectangle(3, 3)
