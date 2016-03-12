0067640de09f4ae86dc98063e39084c2a50188e9
Jack <jack.grahl@yahoo.co.uk>
2016-03-12 11:52:48 +0000

    Accept the size of the square as an argument.

--- make_header.py
+++ make_header.py
@@ -1,6 +1,9 @@
+import sys
+
 from link_data import Rectangle
 
 if __name__ == '__main__':
-    r = Rectangle(13, 13)
+    size = int(sys.argv[1])
+    r = Rectangle(size, size)
     print r.header_text()
 
