e428547e7b40d7ed7eb8994e97e2dc0918c47b8e
Jack <jack.grahl@yahoo.co.uk>
2016-03-14 22:48:00 +0000

    Create a PNG file as output.

--- display_result.py
+++ display_result.py
@@ -1,10 +1,37 @@
+import random
 import re
+
+import png
 
 def print_alpha_blocks(grid):
     chars = '_abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
     table = [ [ chars[n] for n in l ] for l in grid ]
     for row in table:
         print ''.join(row)
+
+def random_color():
+    return (random.randint(0, 255), random.randint(0, 255), random.randint(0, 255))
+
+def create_png(grid, scale=10):
+    d = dict()
+    color_data = []
+    palette = []
+    for r in grid:
+        out_row = []
+        for c in r:
+            if c not in d:
+                d[c] = random_color()
+            for i in range(scale):
+                out_row.append(len(palette))
+            palette.append(d[c])
+        for i in range(scale):
+            color_data.append(out_row)
+    height = len(grid) * scale
+    width = len(grid[0]) * scale
+    writer = png.Writer(width=width, height=height, palette=palette)
+    with open('output.png', 'w') as f:
+        writer.write(f, color_data)
+            
 
 count = 1
 
@@ -27,3 +54,4 @@ for l in f.readlines():
         count = count + 1
             
 print_alpha_blocks(table)
+create_png(table)
