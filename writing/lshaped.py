24ec5d202c4633c61d76e5d9d87ab878acd335d0
Jack <jack.grahl@yahoo.co.uk>
2016-03-21 22:39:03 +0000

    Draw a tetromino.

--- writing/lshaped.py
+++ writing/lshaped.py
@@ -17,6 +17,18 @@ def draw_square(x, y):
 \draw [rounded corners, thick] %s;
 ''' % (path, )
 
+def draw_tetromino(x, y, t):
+    offsets = [(0, 0.5), (1.5, 0.5), (1.5, -0.5), (0.5, -0.5), (0.5, -1.5), (-0.5, -1.5), (-0.5, -0.5), (-1.5, -0.5), (-1.5, 0.5), (0, 0.5)]
+    r = [(1, 0, 0, 1), (0, 1, -1, 0), (-1, 0, 0, -1), (0, -1, 1, 0)][t]
+    rotated = [(ax * r[0] + ay * r[1], ax * r[2] + ay * r[3]) for ax, ay in offsets ]
+    print rotated
+    points = [ (x + a, y + b) for a, b in rotated ]
+    print points
+    path = ' -- '.join([ '(%f, %f)' % p for p in points ])
+    print r'''
+\draw [rounded corners, thick] %s;
+''' % (path, )
+
 if __name__ == '__main__':
     preamble()
 
@@ -24,4 +36,8 @@ if __name__ == '__main__':
     for (x, y) in single_squares:
         draw_square(x, y)
         
+    tetrominos = [(2.5, 1.5, 0)]
+    for x, y, t in tetrominos:
+        draw_tetromino(x, y, t)
+
     postamble()
