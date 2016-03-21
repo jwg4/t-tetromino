693b2037c61774207237bf1439e4f9b0433f3602
Jack <jack.grahl@yahoo.co.uk>
2016-03-21 22:16:31 +0000

    Generate image tikz file.

---
+++ writing/lshaped_image.py
@@ -0,0 +1,27 @@
+single_squares = [(0,0), (0,1), (1,0)]
+
+def preamble():
+    print r'''
+\begin{tikzpicture}
+'''
+
+def postamble():
+    print r'''
+\end{tikzpicture}
+'''
+
+def draw_square(x, y):
+    offsets = [(0, 0.5), (0, 1), (1, 1), (1, 0), (0, 0), (0, 0.5)]
+    points = [ (x + a, y + b) for a, b in offsets ]
+    path = ' -- '.join([ '(%f, %f)' % p for p in points ])
+    print r'''
+\draw [rounded corners, thick] %s;
+''' % (path, )
+
+if __name__ == '__main__':
+    preamble()
+
+    for (x, y) in single_squares:
+        draw_square(x, y)
+        
+    postamble()
