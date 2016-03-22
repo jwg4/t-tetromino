373b05edd0c725e1cb625eba6c4cf50101638ee0
Jack <jack.grahl@yahoo.co.uk>
2016-03-22 07:20:26 +0000

    Add monomino.

--- writing/five.py
+++ writing/five.py
@@ -1,4 +1,4 @@
-from drawings import preamble, postamble, draw_cropped_square, draw_tetromino
+from drawings import preamble, postamble, draw_cropped_square, draw_tetromino, draw_square
 
 if __name__ == '__main__':
     preamble()
@@ -6,4 +6,5 @@ if __name__ == '__main__':
     tetrominos = [(0.5, 3.5, 3), (2.5, 4.5, 0), (2.5, 1.5, 1), (3.5, 1.5, 3), (4.5, 3.5, 1)]
     for x, y, t in tetrominos:
         draw_tetromino(x, y, t)
+    draw_square(1, 2, extras=['pattern = north east lines'])
     postamble()
