d02ca23dc0f4da342e7587c9bc55d7774d9fcdfc
Jack <jack.grahl@yahoo.co.uk>
2016-03-22 07:15:21 +0000

    Add a single tetromino.

--- writing/five.py
+++ writing/five.py
@@ -1,7 +1,9 @@
-
-from drawings import preamble, postamble, draw_cropped_square
+from drawings import preamble, postamble, draw_cropped_square, draw_tetromino
 
 if __name__ == '__main__':
     preamble()
     draw_cropped_square(0, 0, 5)
+    tetrominos = [(0.5, 3.5, 3)]
+    for x, y, t in tetrominos:
+        draw_tetromino(x, y, t)
     postamble()
