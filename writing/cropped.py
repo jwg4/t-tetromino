a7cf64efc1ba2987890916856600051a88bac372
Jack <jack.grahl@yahoo.co.uk>
2016-03-22 07:03:24 +0000

    Add further cropped squares.

--- writing/cropped.py
+++ writing/cropped.py
@@ -3,4 +3,6 @@ from drawings import preamble, postamble, draw_cropped_square
 if __name__ == '__main__':
     preamble()
     draw_cropped_square(0, 0, 5)
+    draw_cropped_square(6, 0, 7)
+    draw_cropped_square(14, 0, 9)
     postamble()
