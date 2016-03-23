372725a9f92146d11bf787be53a706bf1d7ed7e3
Jack <jack.grahl@yahoo.co.uk>
2016-03-23 22:43:10 +0000

    Add all the other tetrominos.

--- writing/threes.py
+++ writing/threes.py
@@ -4,11 +4,14 @@ if __name__ == '__main__':
     preamble()
     draw_cropped_square(0, 2, 7)
     tetrominos = [
-        (2.5, 0.5, 1)
+        (0.5, 2.5, 3), (1.5, 0.5, 2),
+        (7.5, 1.5, 0), (7.5, 2.5, 2),
     ]
     for x, y, t in tetrominos:
         draw_tetromino(x, y, t)
     ghost_tetrominos = [
+        (3.5, 1.5, 0), (5.5, 0.5, 2),
+        (8.5, 4.5, 1), (7.5, 6.5, 3),
     ]
     for x, y, t in ghost_tetrominos:
         draw_tetromino(x, y, t, ['dashed'])
