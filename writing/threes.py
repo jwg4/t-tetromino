d32d200e665b3510d7eaad6a7dbd5ae23266949c
Jack <jack.grahl@yahoo.co.uk>
2016-03-23 22:36:05 +0000

    Start drawing tetrominos in last diagram.

--- writing/threes.py
+++ writing/threes.py
@@ -1,6 +1,15 @@
-from drawings import preamble, postamble, draw_cropped_square
+from drawings import preamble, postamble, draw_cropped_square, draw_tetromino
 
 if __name__ == '__main__':
     preamble()
-    draw_cropped_square(0, 0, 11)
+    draw_cropped_square(2, 0, 11)
+    tetrominos = [
+        (2.5, 0.5, 1)
+    ]
+    for x, y, t in tetrominos:
+        draw_tetromino(x, y, t)
+    ghost_tetrominos = [
+    ]
+    for x, y, t in ghost_tetrominos:
+        draw_tetromino(x, y, t, ['dashed'])
     postamble()
