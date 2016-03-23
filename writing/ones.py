44b94ec0049c691e6945091d3f43320201011c0e
Jack <jack.grahl@yahoo.co.uk>
2016-03-23 22:23:52 +0000

    Start to draw tetrominos.

--- writing/ones.py
+++ writing/ones.py
@@ -1,6 +1,9 @@
-from drawings import preamble, postamble, draw_cropped_square
+from drawings import preamble, postamble, draw_cropped_square, draw_tetromino
 
 if __name__ == '__main__':
     preamble()
-    draw_cropped_square(0, 0, 9)
+    draw_cropped_square(2, 2, 9)
+    tetrominos = [(1.5, 9.5, 1), (0.5, 7.5, 3), (1.5, 5.5, 1), (0.5, 3.5, 3)]
+    for x, y, t in tetrominos:
+        draw_tetromino(x, y, t)
     postamble()
