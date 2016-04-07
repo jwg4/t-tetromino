a619e8754a4e3e227ae8f30171a80956be61cdcf
Jack <jack.grahl@yahoo.co.uk>
2016-04-08 00:27:28 +0100

    Allow a different height and width.

--- make_header.py
+++ make_header.py
@@ -3,7 +3,8 @@ import sys
 from link_data import Rectangle
 
 if __name__ == '__main__':
-    size = int(sys.argv[1])
-    r = Rectangle(size, size)
+    w = int(sys.argv[1])
+    h = int(sys.argv[2])
+    r = Rectangle(w, h)
     print r.header_text()
 
