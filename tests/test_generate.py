deb9bf5e6393a13c122bca5ce8a3004b31742325
Jack <jack.grahl@yahoo.co.uk>
2016-05-26 23:59:18 +0100

    Generate a correct tiling of 4x4.

--- tests/test_generate.py
+++ tests/test_generate.py
@@ -7,4 +7,6 @@ class TestGenerate(unittest.TestCase):
     def test_4_by_4(self):
         tiling = generate_tiling(4, 4)
         self.assertEqual(len(tiling[0]), 4)
+        squares = set([ s for p in tiling[0] for s in p ])
+        self.assertEqual(len(squares), 16)
         
