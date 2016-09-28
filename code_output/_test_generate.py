73c9be955a496df3fc728c730f7f3fc7bca501f5
Jack <jack.grahl@yahoo.co.uk>
2016-09-29 00:24:10 +0100

    Correct name of argument min -> min_value.

--- code_output/_test_generate.py
+++ code_output/_test_generate.py
@@ -8,12 +8,12 @@ from generate import generate_tiling
 
 class TestGenerate(unittest.TestCase):
     @unittest.skip("Can't do arbitrary rectangles yet.")
-    @given(integers(min=1), integers(min=1))
+    @given(integers(min_value=1), integers(min_value=1))
     def test_generate_tiling(self, x, y):
         tiling = generate_tiling(x, y)
         self.assertIsNotNone(tiling)
 
-    @given(integers(min=1))
+    @given(integers(min_value=1))
     def test_generate_square_tiling(self, x):
         tiling = generate_tiling(x, x)
         self.assertIsNotNone(tiling)
