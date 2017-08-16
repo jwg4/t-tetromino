11956ebdaaa6aaf0d03cd4af1277c9a26e9a954f
Jack Grahl <jack.grahl@gmail.com>
2017-08-16 22:41:30 +0000

    Transform the solutions correctly.
    Doinbg this requires knowing what the original problem was.

--- pycover/test/test_transform.py
+++ pycover/test/test_transform.py
@@ -4,17 +4,42 @@ from pycover.transform import MaximalCover
 
 
 class TestMaximalCover(unittest.TestCase):
+    problem = [
+        [1, 1, 0, 0],
+        [0, 0, 1, 1],
+        [0, 1, 0, 1],
+        [1, 0, 1, 0],
+    ]
+    problem_2 = [
+        [1, 1, 1, 0],
+        [0, 1, 1, 1],
+        [1, 1, 0, 1],
+        [1, 0, 1, 1],
+    ]
+
     def test_transform_problem(self):
-        problem = [
-            [1, 1, 0, 0],
-            [0, 0, 1, 1],
-            [0, 1, 0, 1],
-            [1, 0, 1, 0],
-        ]
-        transformed_problem = MaximalCover.f(problem)
+        transformed_problem = MaximalCover.f(self.problem)
         self.assertEqual(len(transformed_problem), 8)
 
     def test_transform_solution(self):
-        solution = [0, 1]
-        transformed_solution = MaximalCover.g(solution)
-        self.assertEqual(transformed_solution, solution)
+        solution = [
+            [1, 0, 0, 0, 1, 1, 0, 0],
+            [0, 1, 0, 0, 0, 0, 1, 1],
+        ]
+        transformed_solution = MaximalCover.g(solution, self.problem)
+        self.assertEqual(len(transformed_solution), len(solution))
+        
+    def test_transform_problem_2(self):
+        transformed_problem = MaximalCover.f(self.problem_2)
+        self.assertEqual(len(transformed_problem), 8)
+
+    def test_transform_solution_2(self):
+        solution = [
+            [1, 0, 0, 0, 1, 1, 1, 0],
+            [0, 1, 1, 1, 0, 0, 0, 1]
+        ]
+        transformed_solution = MaximalCover.g(solution, self.problem_2)
+        expected = [
+            [1, 1, 1, 0],
+        ]
+        self.assertEqual(transformed_solution, expected)
--- pycover/transform.py
+++ pycover/transform.py
@@ -1,7 +1,7 @@
 def make_monad(f, g):
     def monad(x):
         def y(a):
-            return g(x(f(a)))
+            return g(x(f(a)), a)
         return y
 
     monad.f = f
@@ -35,8 +35,18 @@ def MaximalCoverProblem(problem):
     return list(generate_max_cover_problem(problem))
 
 
-def MaximalCoverSolution(solution):
+def generate_max_cover_solution(solution, problem):
+    n_sets = len(problem[0])
+
+    for row in solution:
+        count = len([bit for bit in row[0:n_sets] if bit == 1])
+        if count == 1:
+            yield row[n_sets:]
     return solution
+
+
+def MaximalCoverSolution(solution, problem):
+    return list(generate_max_cover_solution(solution, problem))
 
 
 MaximalCover = make_monad(MaximalCoverProblem, MaximalCoverSolution)
