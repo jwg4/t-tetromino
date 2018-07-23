e256fc02a4b7ebe27441cc24578a0e069bc4df96
Jack Grahl <jack@prismfp.com>
2018-07-23 17:14:18 +0100

    Print the values from YAML in the table.

--- writing/table.py
+++ writing/table.py
@@ -9,16 +9,33 @@ def read_data(filename):
     return data
 
 
-def start_line(data):
+def data_lines(data):
+    for d in data:
+        header = str(d.pop('name'))
+        values = [ str(d[k]) for k in sorted(d.keys()) ]
+        value_str = " & ".join([header] + values) + r" \\"
+        yield value_str
+        
+
+def start_lines(data):
     col_num = len(data[0])
     format_str = "|".join(["r"] + ["c"] * (col_num))
-    return r"\begin{tabular}{%s}" % (format_str, )
+    yield r"\begin{tabular}{%s}" % (format_str, )
+
+    values = [ str(k) for k in sorted(data[0].keys()) if k != 'name']
+    value_str = " & ".join([""] + values) + r" \\"
+    yield value_str 
+
+    yield "\hline"
 
 
 if __name__ == '__main__':
     filename = sys.argv[1]
 
     data = read_data(filename)
-    print start_line(data)
+    for line in start_lines(data):
+        print line
+    for line in data_lines(data):
+        print line
     print "\end{tabular}"
     
