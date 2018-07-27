8b9a1bcc2635e0d0f2ac544b8102fc2f451ea785
Jack Grahl <jack@prismfp.com>
2018-07-27 11:25:45 +0100

    Draw the tetrominos on the left and right sides.

--- writing/extending.py
+++ writing/extending.py
@@ -3,21 +3,20 @@ from drawings import preamble, postamble, draw_tetromino, draw_rectangle
 if __name__ == '__main__':
     preamble()
 
-    tetrominos = [(2.5, 1.5, 0), (16.5, 0.5, 2), (17.5, 2.5, 1), (16.5, 16.5, 3)]
+    tetrominos = [
+        (0.5, 1.5, 3), (1.5, 7.5, 1), (0.5, 9.5, 3),
+        (8.5, 1.5, 1), (7.5, 7.5, 3), (8.5, 9.5, 1),
+    ]
     for x, y, t in tetrominos:
-        pass
-        #draw_tetromino(x, y, t)
+        draw_tetromino(x, y, t)
     
     ghost_tetrominos = [
-        (6.5, 1.5, 0), (4.5, 0.5, 2),
-        (14.5, 1.5, 0), (12.5, 0.5, 2),
-        (17.5, 6.5, 1), (16.5, 4.5, 3),
-        (17.5, 14.5, 1), (16.5, 12.5, 3)
+        (1.5, 3.5, 1), (0.5, 5.5, 3),
+        (7.5, 3.5, 3), (8.5, 5.5, 1),
     ]
     for x, y, t in ghost_tetrominos:
-        pass
-        #draw_tetromino(x, y, t, ['dashed'])
+        draw_tetromino(x, y, t, ['dashed'])
 
-    draw_rectangle(0, 2, 5, 7, ['pattern = north east lines'])
+    draw_rectangle(2, 2, 5, 7, ['pattern = north east lines'])
 
     postamble()
