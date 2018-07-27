433cb7d2c2afbd3bd3aa2f1d9c1315102cb21cff
Jack Grahl <jack@prismfp.com>
2018-07-27 11:39:29 +0100

    Complete the proof by picture.

--- writing/extending.py
+++ writing/extending.py
@@ -5,14 +5,18 @@ if __name__ == '__main__':
 
     tetrominos = [
         (0.5, 1.5, 3), (1.5, 7.5, 1), (0.5, 9.5, 3),
+        (2.5, 0.5, 2),
         (8.5, 1.5, 1), (7.5, 7.5, 3), (8.5, 9.5, 1),
+        (2.5, 10.5, 0),
     ]
     for x, y, t in tetrominos:
         draw_tetromino(x, y, t)
     
     ghost_tetrominos = [
         (1.5, 3.5, 1), (0.5, 5.5, 3),
+        (4.5, 1.5, 0), (6.5, 0.5, 2),
         (7.5, 3.5, 3), (8.5, 5.5, 1),
+        (4.5, 9.5, 2), (6.5, 10.5, 0)
     ]
     for x, y, t in ghost_tetrominos:
         draw_tetromino(x, y, t, ['dashed'])
