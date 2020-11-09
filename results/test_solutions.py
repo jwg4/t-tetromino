0d5460afd541b396afb83cd42836420dba41082f
Jack Grahl <jack.grahl@gmail.com>
2020-11-09 18:24:01 +0000

    Configure the tests to stop them killing the machine.

--- results/test_solutions.py
+++ results/test_solutions.py
@@ -1,10 +1,11 @@
-from hypothesis import given
+from hypothesis import given, settings
 from hypothesis.strategies import integers
 from pytest import mark
 
 from solution import get_solution
 
 MAX_VALUE = 3
+REAL_MAX_VALUE = 100
 
 
 @given(integers(min_value=1, max_value=MAX_VALUE), integers(min_value=1, max_value=MAX_VALUE))
@@ -15,7 +16,7 @@ def test_all_solutions(x, y):
     assert solution.count <= 6
 
 
-@given(integers(min_value=1, max_value=MAX_VALUE))
+@given(integers(min_value=1, max_value=REAL_MAX_VALUE))
 def test_all_strips_of_width_2(x):
     solution = get_solution(x, 2)
     assert solution is not None
@@ -23,15 +24,15 @@ def test_all_strips_of_width_2(x):
     assert solution.count <= 4
 
 
-@mark.skip('We have these results in our paper')
-@given(integers(min_value=1))
+@given(integers(min_value=1, max_value=REAL_MAX_VALUE))
 def test_all_squares(size):
-    solution = get_solution(size)
+    solution = get_solution(size, size)
     assert solution is not None
     assert solution.count <= 5
 
 
-@given(integers(min_value=1), integers(min_value=1))
+@settings(deadline=None)
+@given(integers(min_value=1, max_value=REAL_MAX_VALUE), integers(min_value=1, max_value=REAL_MAX_VALUE))
 def test_all_perfect_solutions(x, y):
     solution = get_solution(4 * x, 4 * y)
     assert solution is not None
@@ -39,7 +40,7 @@ def test_all_perfect_solutions(x, y):
     assert solution.count == 0
     
     
-@given(integers(min_value=1))
+@given(integers(min_value=1, max_value=REAL_MAX_VALUE))
 def test_all_doubled_odd_squares(k):
     solution = get_solution(2 * k + 1, 4 * k + 2)
     assert solution is not None
@@ -47,7 +48,7 @@ def test_all_doubled_odd_squares(k):
     assert solution.count == 2
     
     
-@given(integers(min_value=1), integers(min_value=0))
+@given(integers(min_value=1, max_value=REAL_MAX_VALUE), integers(min_value=0, max_value=REAL_MAX_VALUE))
 def test_all_augmented_doubled_odd_squares(k, m):
     solution = get_solution(2 * k + 2 * m + 1, 4 * k  + 2 * m + 2)
     assert solution is not None
