8480b07c2b1a7c8f112eb54c65c888ba0b634671
Jack Grahl <jack.grahl@gmail.com>
2020-11-09 22:38:52 +0000

    A cleaner condition.

--- results/solution.py
+++ results/solution.py
@@ -7,7 +7,7 @@ from transform import TransposedResult
 def get_solution(x, y):
     if x < y:
         return TransposedResult(get_solution(y, x))
-    elif x < 2 or y < 2 or x * y < 6:
+    elif x < 2 or y < 2 or (x == 2 and y == 2):
         return EnumeratedResult.empty(x, y)
     elif x == 3 and y in [2, 3]:
         return EnumeratedResult(x, y, [[(0, 0), (1, 0), (2, 0), (1, 1)]])
