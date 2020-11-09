3dffccb02910bf88359f69a4cb32205065308e03
Jack Grahl <jack.grahl@gmail.com>
2020-11-09 17:32:23 +0000

    Vastly improve the checking.

--- results/check.py
+++ results/check.py
@@ -1,8 +1,36 @@
 def check_result(x, y, tiles):
+    seen = set()
     for tile in tiles:
+        if not is_t_tetromino(tile):
+            return False
         for s in tile:
             if s[0] >= x:
                 return False
             if s[1] >= y:
                 return False
+            if s[0] < 0:
+                return False
+            if s[1] < 0:
+                return False
+            if s in seen:
+                return False
+            else:
+                seen.add(s)
     return True
+
+
+def is_t_tetromino(tile):
+    if len(tile) != 4:
+        return False
+    if len(set(tile)) != 4:
+        return False
+    for square in tile:
+        if all(_is_adjacent_or_same(square, other_square) for other_square in tile):
+            return True
+    return False
+
+
+def _is_adjacent_or_same(square, other_square):
+    x, y = square
+    ox, oy = other_square
+    return abs(x - ox) + abs(y - oy) <= 1
--- results/test_check.py
+++ results/test_check.py
@@ -1,7 +1,7 @@
 from hypothesis import given
 from hypothesis.strategies import integers
 
-from check import check_result
+from check import check_result, is_t_tetromino
 
 
 @given(integers(), integers())
@@ -9,5 +9,35 @@ def test_empty(x, y):
     assert check_result(x, y, [])
     
 
+def test_looks_fine():
+    assert check_result(2, 3, [[(0, 0), (0, 1), (1, 1), (0, 2)]])
+
+
 def test_tile_over_the_edge():
     assert not check_result(2, 2, [[(0, 0), (0, 1), (1, 1), (0, 2)]])
+    
+
+def test_tile_over_the_other_edge():
+    assert not check_result(2, 2, [[(0, 0), (0, 1), (1, 1), (-1, 1)]])
+    
+
+def test_overlapping():
+    assert not check_result(3, 2, [[(0, 0), (1, 0), (1, 1), (2, 0)], [(0, 1), (1, 1), (1, 0), (2, 1)]])
+
+
+def test_non_t_tetromino():
+    assert not check_result(2, 2, [[(0, 0), (0, 1), (1, 1), (1, 0)]])
+
+
+def test_not_even_tetromino():
+    assert not check_result(2, 2, [[(0, 0), (0, 1), (1, 1)]])
+    assert not check_result(3, 3, [[(0, 0), (0, 1), (1, 1), (0, 2), (-1, 1)]])
+    assert not check_result(3, 3, [[(1, 0), (1, 1), (2, 1), (1, 2), (0, 1)]])
+    
+
+def test_4_square_not_distinct():
+    assert not is_t_tetromino([(0, 0), (1, 1), (0, 1), (1, 1)])
+    
+
+def test_5_square_with_4_distinct():
+    assert not is_t_tetromino([(0, 0), (1, 1), (0, 1), (1, 1), (0, 2)])
