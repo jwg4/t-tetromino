a35030e9f545871b4c19c1bfb1020dcce0aa75b2
Jack Grahl <jack.grahl@gmail.com>
2020-11-14 17:51:42 +0000

    doubled square arguments.

--- results/solution.py
+++ results/solution.py
@@ -2,7 +2,7 @@ from basic import EnumeratedResult, Strip, Perfect
 from compound import SimpleLShape, ConcatenatedResult, AugmentedResult
 from odd_rectangle import strip_of_height_15
 from odd_square import make_odd_square
-from transform import TransposedResult, transpose, rotate
+from transform import TransposedResult, transpose, rotate, reflect
 
 
 def get_solution(x, y):
@@ -46,9 +46,25 @@ def get_solution(x, y):
                 ),
                 Strip(x)
             ),
-            []
-        )
-                
+            [
+                [(y-3, y-4), (y-4, y-3), (y-3, y-3), (y-2, y-3)],
+                [(x-1, y-4), (x-2, y-3), (x-1, y-3), (x-1, y-2)],
+            ]
+        )       
+    elif x == 2 * y + 2 and y % 2 == 1:
+        return AugmentedResult(
+            ConcatenatedResult.AlongXAxis(
+                ConcatenatedResult.AlongXAxis(
+                    make_odd_square(y),
+                    make_odd_square(y)
+                ),
+                reflect(transpose(Strip(y)), False)
+            ),
+            [
+                [(y-2, y-1), (y-1, y-1), (y-1, y-2), (y, y-1)],
+                [(x-4, y-1), (x-3, y-1), (x-3, y-2), (x-2, y-1)]
+            ]
+        )                
     elif x == 4 and y == 3:
         return EnumeratedResult(4, 3, 
             [[(0, 0), (1, 0), (1, 1), (2, 0)], [(3, 0), (2, 1), (3, 1), (3, 2)]]
