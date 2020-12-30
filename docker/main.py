d08ea25ddf0f97e4b65c5dd4ebd58cb3010567bb
Jack Grahl <jack.grahl@gmail.com>
2020-12-30 16:33:00 +0000

    Correctly write file to a fixed location.

--- docker/main.py
+++ docker/main.py
@@ -8,5 +8,5 @@ if __name__ == '__main__':
     timestamp = datetime.utcnow()
     rint = randint(1, 1000000)
 
-    with open("output.txt") as f:
+    with open("output.txt", "w") as f:
         f.write("Timestamp: %s, Random int: %d" % (timestamp, rint))
