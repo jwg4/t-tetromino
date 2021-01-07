d980183361e5e84f4da96b9869fea9e505dea12e
Jack Grahl <jack.grahl@gmail.com>
2021-01-07 23:15:03 +0000

    Update.

--- docker_config/azure.sh
+++ docker_config/azure.sh
@@ -1,8 +1,9 @@
 ACR_LOGIN_SERVER=ttdocker.azurecr.io
 RES_GROUP=t-tetromino
-TAG=aeda7142
+TAG=27d583f1
 AKV_NAME=ttkeyvalut
 ACR_NAME=ttdocker
 
 az container create --resource-group $RES_GROUP --name mycontainer --image $ACR_NAME.azurecr.io/tt_repository/minimal:$TAG --registry-login-server $ACR_LOGIN_SERVER --registry-username $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-usr --query value -o tsv) --registry-password $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-pwd --query value -o tsv) --restart-policy Never
 
+az container show --resource-group $RES_GROUP --name mycontainer --out table
