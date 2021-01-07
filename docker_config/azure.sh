3bf063dde98412c22e21bff2df70e6e7858a8947
Jack Grahl <jack.grahl@gmail.com>
2021-01-07 23:01:20 +0000

    Add restart policy.

--- docker_config/azure.sh
+++ docker_config/azure.sh
@@ -4,5 +4,5 @@ TAG=aeda7142
 AKV_NAME=ttkeyvalut
 ACR_NAME=ttdocker
 
-az container create --resource-group $RES_GROUP --name mycontainer --image $ACR_NAME.azurecr.io/tt_repository/minimal:$TAG --registry-login-server $ACR_LOGIN_SERVER --registry-username $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-usr --query value -o tsv) --registry-password $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-pwd --query value -o tsv)
+az container create --resource-group $RES_GROUP --name mycontainer --image $ACR_NAME.azurecr.io/tt_repository/minimal:$TAG --registry-login-server $ACR_LOGIN_SERVER --registry-username $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-usr --query value -o tsv) --registry-password $(az keyvault secret show --vault-name $AKV_NAME -n $ACR_NAME-pull-pwd --query value -o tsv) --restart-policy Never
 
