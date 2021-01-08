17a52d125024eae8896551267d700d5792ba5df0
Jack Grahl <jack.grahl@gmail.com>
2021-01-08 19:33:30 +0000

    Write to the share.

--- docker_config/main.py
+++ docker_config/main.py
@@ -3,13 +3,16 @@
 from datetime import datetime
 from random import randint
 
+FOLDER = "/aci/output"
+
 
 if __name__ == '__main__':
     timestamp = datetime.utcnow()
     rint = randint(1, 1000000)
 
     output_string = "Timestamp: %s, Random int: %d" % (timestamp, rint)
+    destination = os.path.join(FOLDER, "output.txt")
 
-    with open("output.txt", "w") as f:
+    with open(destination, "w") as f:
         f.write(output_string)
     print(output_string)
