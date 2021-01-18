f1553b40bcb9d8eea025d373b398e8bffcaa4b5b
Jack Grahl <jack.grahl@gmail.com>
2021-01-18 23:38:26 +0000

    Add a helper to list all the tasks.

---
+++ helpers/list_tasks.py
@@ -0,0 +1,22 @@
+import json
+import os
+
+
+def get_files(folder):
+    for f in os.listdir(folder):
+        if f.endswith(".py"):
+            yield f
+
+    
+if __name__ == '__main__':
+    FOLDER = "docker_config/tasks"
+    data = {
+        "include": [
+            {"taskfile": filename}
+            for filename in get_files(FOLDER)
+        ]
+    }
+
+    data_json = json.dumps(data) 
+    s = "::set-output name=matrix::%s" % data_json
+    print(s)
