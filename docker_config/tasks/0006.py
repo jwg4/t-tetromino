cc3abff7c66c4525775ce3c66552250974a6ab5d
Jack Grahl <jack.grahl@gmail.com>
2021-01-18 23:15:11 +0000

    Add all the new tasks.

---
+++ docker_config/tasks/0006.py
@@ -0,0 +1,19 @@
+LEFT_CAP_13_HEIGHTS = [
+    0, 1, 0, 1, -1, -1, -1, -1, -1, -2, -1, -2, -1
+]
+
+
+def gen_13_left_cap(n):
+    for i in range(0, 13):
+        h = LEFT_CAP_13_HEIGHTS[i]
+        for j in range(0, n + h):
+            yield (i, j)
+
+
+def make_13_left_cap(n):
+    return list(gen_13_left_cap(n))
+
+ID = "0006"
+MCOUNT = 1
+BOARD = make_13_left_cap(30)
+
