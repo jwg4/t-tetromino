5574bf5622b99264b881791825ca013ae86a0f7d
Jack Grahl <jack.grahl@gmail.com>
2021-01-20 00:33:26 +0000

    Output Python code.

--- docker_config/main.py
+++ docker_config/main.py
@@ -9,13 +9,16 @@ from polyomino.tileset import many
 from spec import ID, MCOUNT, BOARD
 
 
-def full_output(solution):
-    yield "ID: %s" % (ID, )
-    yield "Timestamp: %s" % (datetime.utcnow())
+def full_output(solution, start_time):
+    yield "# ID: %s" % (ID, )
+    yield "# Start time: %s" % (start_time, )
+    yield "# Timestamp: %s" % (datetime.utcnow(), )
 
-    yield repr(solution.tiling)
+    yield "TILING = %s" % (repr(solution.tiling), )
 
+    yield "DISPLAY = '''"
     yield solution.display()
+    yield "'''"
 
 
 def make_board():
@@ -23,11 +26,13 @@ def make_board():
 
 
 if __name__ == '__main__':
+    start_time = datetime.utcnow()
+
     board = make_board()
     tileset = many(TETROMINOS['T']).and_repeated_exactly(MCOUNT, MONOMINO)
     problem = board.tile_with_set(tileset)
     solution = problem.solve()
-    output_string = "\n".join(full_output(solution))
+    output_string = "\n".join(full_output(solution, start_time))
 
     FOLDER = os.environ['DEST_FOLDER']
     filename =  "output_%s.txt" % (ID, )
