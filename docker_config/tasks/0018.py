05b324b67f5239e6197596c4cea7997476c49d72
Jack Grahl <jack.grahl@gmail.com>
2021-01-23 12:10:05 +0000

    Move the notches. [build]

---
+++ docker_config/tasks/0018.py
@@ -0,0 +1,21 @@
+RIGHT_CAP_13_HEIGHTS = [
+    2, 3, 2, 3, 2, 2, 2, 2, 2, 0, 1, 0, 1
+]
+
+
+def gen_13_right_cap_with_notch(n, remove):
+    for i in range(0, 13):
+        h = RIGHT_CAP_13_HEIGHTS[i]
+        for j in range(0, n + h - 1):
+            if i != remove[0] or j != remove[1]:
+                yield (i, j)
+
+
+def make_13_right_cap(n, remove):
+    return list(gen_13_right_cap(n, remove))
+
+ID = "0018"
+NAME = "right_cap_13_height_12_notch_4"
+MCOUNT = 0
+BOARD = make_13_right_cap(12, (0, 4))
+
