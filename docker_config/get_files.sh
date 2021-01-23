7d300f5d9d74d1f1160e64c6cdaa73935c7d4455
Jack Grahl <jack.grahl@gmail.com>
2021-01-23 11:01:22 +0000

    Detect the output files.

--- docker_config/get_files.sh
+++ docker_config/get_files.sh
@@ -4,6 +4,11 @@ ACI_SHARE_NAME=search-output
 
 STORAGE_KEY=$(az storage account keys list --resource-group $RES_GROUP --account-name $ACI_STORAGE_ACCOUNT_NAME --query "[0].value" --output tsv)
 
-ID="0002"
-
-az storage file download --share-name $ACI_SHARE_NAME --account-name $ACI_STORAGE_ACCOUNT_NAME --account-key=$STORAGE_KEY --path "output_${ID}.txt" --dest "results/output_${ID}.txt" --output none
+az storage file list --share-name $ACI_SHARE_NAME --account-name $ACI_STORAGE_ACCOUNT_NAME --account-key=$STORAGE_KEY --output tsv | cut -f2 | while read -r filename ; do
+    if [ -f "results/$filename" ]; then
+        echo "$filename exists"
+    else
+        echo "Downloading $filename"
+        az storage file download --share-name $ACI_SHARE_NAME --account-name $ACI_STORAGE_ACCOUNT_NAME --account-key=$STORAGE_KEY --path "$filename" --dest "results/$filename" --output none
+    fi
+done
